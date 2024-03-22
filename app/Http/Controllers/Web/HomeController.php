f8859ba2a819ab3743c05454a65df63bf3adff74
Costin Urse <costin.urse@mrm.com>
2024-03-22 22:51:38 +0200

    add gd

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return redirect()->route('login');
+        //return redirect()->route('login');
+        return view('home');
     }
 }
